af3a3a9d95b80cd9a3efc45c18f5fbef9439d56c
Kosikov Nikita <green.marker2@mail.ru>
2021-06-04 11:53:08 +0300

    Include List library

--- persistence/src/main/java/com/epam/esm/GiftCertificateDAO.java
+++ persistence/src/main/java/com/epam/esm/GiftCertificateDAO.java
@@ -1,5 +1,7 @@
 package com.epam.esm;
 
+import java.util.List;
+
 public interface GiftCertificateDAO {
     void insert(GiftCertificate giftCertificate);
     void update(GiftCertificate giftCertificate, int id);
