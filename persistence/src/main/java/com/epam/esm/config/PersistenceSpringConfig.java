311e7489d3845bcc71e45efc813fb69521d83336
Kosikov Nikita <green.marker2@mail.ru>
2021-06-04 17:20:44 +0300

    Changed PersistenceSpringConfig, added bean dataSource

--- persistence/src/main/java/com/epam/esm/config/PersistenceSpringConfig.java
+++ persistence/src/main/java/com/epam/esm/config/PersistenceSpringConfig.java
@@ -6,14 +6,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("com.epam.esm")
 public class PersistenceSpringConfig {
 
     @Bean
+    public DataSource beanDataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/module_2");
+        dataSource.setUsername("Nikita");
+        dataSource.setPassword("knyazrek2");
+        return dataSource;
+    }
+
+    @Bean
     public JdbcTemplate beanJdbcTemplate(){
-        return new JdbcTemplate();
+        return new JdbcTemplate(beanDataSource());
     }
 
     @Bean
