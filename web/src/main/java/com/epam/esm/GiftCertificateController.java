ec1d1f1644ecfc3beecea4b00b967a29956bafdc
Kosikov Nikita <green.marker2@mail.ru>
2021-06-04 12:25:30 +0300

    Added GiftCertificateController class

---
+++ web/src/main/java/com/epam/esm/GiftCertificateController.java
@@ -0,0 +1,62 @@
+package com.epam.esm;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+public class GiftCertificateController {
+
+    private final GiftCertificateDAO giftCertificateDAO;
+
+    @Autowired
+    public GiftCertificateController(GiftCertificateDAO giftCertificateDAO) {
+        this.giftCertificateDAO = giftCertificateDAO;
+    }
+
+    @GetMapping("giftCertificates")
+    public String getAllGiftCertificates(Model model){
+        List<GiftCertificate> giftCertificates = giftCertificateDAO.read();
+        model.addAttribute(giftCertificates);
+        return "show_gift_certificates.jsp";
+    }
+
+    @PostMapping("/giftCertificates")
+    public String createGiftCertificate(@ModelAttribute GiftCertificate giftCertificate){
+        giftCertificateDAO.insert(giftCertificate);
+        return "show_gift_certificates.jsp";
+    }
+
+    @GetMapping("/giftCertificates/new")
+    public String formForCreateGiftCertificate(){
+        return "form_create_gift_certificate.jsp";
+    }
+
+    @GetMapping("/giftCertificates/{id}/edit")
+    public String formForEditGiftCertificate(@PathVariable String id, Model model){
+        model.addAttribute("gift_certificate_id", id);
+        return "form_edit_gift_certificate.jsp";
+    }
+
+    @GetMapping("/giftCertificates/{id}")
+    public String getGiftCertificate(@PathVariable int id, Model model){
+        GiftCertificate giftCertificate = giftCertificateDAO.readById(id);
+        model.addAttribute(giftCertificate);
+        return "show_gift_certificate.jsp";
+    }
+
+    @PatchMapping("/giftCertificates/{id}")
+    public String updateGiftCertificate(@ModelAttribute GiftCertificate giftCertificate, @PathVariable int id){
+        giftCertificateDAO.update(giftCertificate, id);
+        return "show_gift_certificates.jsp";
+    }
+
+    @DeleteMapping("/giftCertificates/{id}")
+    public String deleteGiftCertificate(@PathVariable int id){
+        giftCertificateDAO.delete(id);
+        return "show_gift_certificates.jsp";
+    }
+}
