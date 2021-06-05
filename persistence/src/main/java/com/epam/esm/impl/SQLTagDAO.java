33dc62e62b67e3792e9eccad9afd4dd3f99e0e32
Kosikov Nikita <green.marker2@mail.ru>
2021-06-05 14:00:36 +0300

    Changed SQLTAGDAO fields

--- persistence/src/main/java/com/epam/esm/impl/SQLTagDAO.java
+++ persistence/src/main/java/com/epam/esm/impl/SQLTagDAO.java
@@ -3,16 +3,17 @@ package com.epam.esm.impl;
 import com.epam.esm.Tag;
 import com.epam.esm.TagDAO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class SQLTagDAO implements TagDAO {
 
-    private static final String DB_NAME = "name";
-
     private static final String INSERT_TAG = "INSERT INTO tag (name) VALUES (?)";
-    private static final String UPDATE_TAG = "UPDATE tag SET name=?";
+    private static final String UPDATE_TAG = "UPDATE tag SET name=? WHERE id=?";
     private static final String DELETE_TAG_BY_ID = "DELETE FROM tag WHERE id=?";
     private static final String FIND_ALL_TAG = "DELETE FROM tag WHERE id=?";
 
@@ -29,8 +30,8 @@ public class SQLTagDAO implements TagDAO {
     }
 
     @Override
-    public void update(String name) {
-        jdbcTemplate.update(UPDATE_TAG, name);
+    public void update(String name, int id) {
+        jdbcTemplate.update(UPDATE_TAG, name, id);
     }
 
     @Override
@@ -39,10 +40,7 @@ public class SQLTagDAO implements TagDAO {
     }
 
     @Override
-    public void read() {
-        jdbcTemplate.query(FIND_ALL_TAG, (rs, rowNum) -> {
-            String name = rs.getString(DB_NAME);
-            return new Tag(name);
-        });
+    public List<Tag> read() {
+        return jdbcTemplate.query(FIND_ALL_TAG, new BeanPropertyRowMapper<>(Tag.class));
     }
 }
