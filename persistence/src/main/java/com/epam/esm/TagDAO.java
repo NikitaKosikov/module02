5c7a038fbf8e92c85c3b5dce58d2a62909ed44b0
Kosikov Nikita <green.marker2@mail.ru>
2021-06-05 14:12:46 +0300

    Update methods

--- persistence/src/main/java/com/epam/esm/TagDAO.java
+++ persistence/src/main/java/com/epam/esm/TagDAO.java
@@ -3,8 +3,9 @@ package com.epam.esm;
 import java.util.List;
 
 public interface TagDAO {
-    void insert(String name);
-    void update(String name, int id);
+    void insert(Tag tag);
+    void update(Tag tag, int id);
     void delete(int id);
     List<Tag> read();
+    Tag readById(int id);
 }
