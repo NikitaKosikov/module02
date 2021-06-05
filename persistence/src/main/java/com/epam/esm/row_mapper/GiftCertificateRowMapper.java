89aa817377a3fa82ed0433b09e822f34b633061b
Kosikov Nikita <green.marker2@mail.ru>
2021-06-05 13:40:48 +0300

    Created GiftCertificateRowMapper class

---
+++ persistence/src/main/java/com/epam/esm/row_mapper/GiftCertificateRowMapper.java
@@ -0,0 +1,20 @@
+package com.epam.esm;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class GiftCertificateRowMapper implements RowMapper<GiftCertificate> {
+    @Override
+    public GiftCertificate mapRow(ResultSet rs, int rowNum) throws SQLException {
+        GiftCertificate giftCertificate = new GiftCertificate();
+        giftCertificate.setName(rs.getString("name"));
+        giftCertificate.setDescription(rs.getString("description"));
+        giftCertificate.setDuration(rs.getInt("duration"));
+        giftCertificate.setPrice(rs.getBigDecimal("price"));
+        giftCertificate.setCreateDate(rs.getDate("create_date"));
+        giftCertificate.setLastUpdateDate(rs.getDate("last_update_date"));
+        return giftCertificate;
+    }
+}
