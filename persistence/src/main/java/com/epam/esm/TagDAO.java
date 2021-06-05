2d12091e73a1779536a9e8b6e4c955bee4500beb
Kosikov Nikita <green.marker2@mail.ru>
2021-06-05 13:57:51 +0300

    Changed return value of method read

--- persistence/src/main/java/com/epam/esm/TagDAO.java
+++ persistence/src/main/java/com/epam/esm/TagDAO.java
@@ -1,8 +1,10 @@
 package com.epam.esm;
 
+import java.util.List;
+
 public interface TagDAO {
     void insert(String name);
-    void update(String name);
+    void update(String name, int id);
     void delete(int id);
-    void read();
+    List<Tag> read();
 }
