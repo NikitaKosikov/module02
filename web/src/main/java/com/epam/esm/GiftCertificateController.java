ed4f1e4ec760d5aca309161daadb63ed99115ed4
Kosikov Nikita <green.marker2@mail.ru>
2021-06-06 11:44:09 +0300

    Added dateTime ISO 8601

--- web/src/main/java/com/epam/esm/GiftCertificateController.java
+++ web/src/main/java/com/epam/esm/GiftCertificateController.java
@@ -1,67 +1,85 @@
 package com.epam.esm;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
-
-import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
+import java.util.TimeZone;
 
 @Controller
 @RequestMapping(value = "/giftCertificates")
 public class GiftCertificateController {
 
-    private final GiftCertificateDAO giftCertificateDAO;
+    private final GiftCertificateService giftCertificateService;
 
     @Autowired
-    public GiftCertificateController(@Qualifier("SQLGiftCertificateDAO") GiftCertificateDAO giftCertificateDAO) {
-        this.giftCertificateDAO = giftCertificateDAO;
+    public GiftCertificateController(GiftCertificateService giftCertificateService) {
+        this.giftCertificateService = giftCertificateService;
     }
-//++
+
     @GetMapping
     public String getAllGiftCertificates(Model model){
-        List<GiftCertificate> giftCertificates = giftCertificateDAO.read();
+        List<GiftCertificate> giftCertificates = giftCertificateService.read();
         model.addAttribute("gift_certificates", giftCertificates);
         return "show_gift_certificates";
     }
-//++
+
     @PostMapping
-    public String createGiftCertificate(@ModelAttribute GiftCertificate giftCertificate){
-        giftCertificateDAO.insert(giftCertificate);
+    public String createGiftCertificate(@ModelAttribute GiftCertificate giftCertificate) throws ParseException {
+        DateFormat dateFormat = getDateTimeIso8610();
+        String nowDateTime = dateFormat.format(new Date());
+
+        giftCertificate.setCreateDate(dateFormat.parse(nowDateTime));
+        giftCertificate.setLastUpdateDate(dateFormat.parse(nowDateTime));
+
+        giftCertificateService.insert(giftCertificate);
         return "redirect:/giftCertificates/new";
     }
-//++
+
     @GetMapping("/new")
     public String formForCreateGiftCertificate(){
         return "/form_create_gift_certificate";
     }
-//++
+
     @GetMapping("/{id}/edit")
     public String formForEditGiftCertificate(@PathVariable int id, Model model){
-        model.addAttribute("certificate", giftCertificateDAO.readById(id));
+        model.addAttribute("certificate", giftCertificateService.readById(id));
         return "/form_edit_gift_certificate";
     }
-//++
+
     @GetMapping("/{id}")
     public String getGiftCertificate(@PathVariable int id, Model model){
-        GiftCertificate giftCertificate = giftCertificateDAO.readById(id);
+        GiftCertificate giftCertificate = giftCertificateService.readById(id);
         model.addAttribute("certificate", giftCertificate);
         return "/show_gift_certificate";
     }
 
-    //++
+
     @PatchMapping("/{id}")
-    public String updateGiftCertificate(@ModelAttribute GiftCertificate giftCertificate, @PathVariable int id){
-        giftCertificateDAO.update(giftCertificate, id);
+    public String updateGiftCertificate(@ModelAttribute GiftCertificate giftCertificate, @PathVariable int id) throws ParseException {
+        DateFormat dateFormat = getDateTimeIso8610();
+        String nowDateTime = dateFormat.format(new Date());
+        giftCertificate.setLastUpdateDate(dateFormat.parse(nowDateTime));
+        giftCertificateService.update(giftCertificate, id);
         return "redirect:/gift_certificates/" + id;
     }
-    //++
+
     @DeleteMapping("/{id}")
     public String deleteGiftCertificate(@PathVariable int id){
-        giftCertificateDAO.delete(id);
+        giftCertificateService.delete(id);
         return "redirect:/giftCertificates";
+    }
+
+    private DateFormat getDateTimeIso8610(){
+        TimeZone tz = TimeZone.getTimeZone("UTC");
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
+        df.setTimeZone(tz);
+        return df;
     }
 }
